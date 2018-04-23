ffc725a95d2f98b4c0fb50b63dc7cb4c008cf183
Colin Russell <colinrussell101@gmail.com>
2018-04-23 15:03:05 -0400

    added image views to scroll view but need to setup constraints

--- ScrollView Image Galleries/ViewController.m
+++ ScrollView Image Galleries/ViewController.m
@@ -10,20 +10,44 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupImageViews];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupImageViews {
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-in-Field.jpg"]];
+    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-night.jpg"]];
+    UIImageView *imageView3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse.jpg"]];
+    imageView1.translatesAutoresizingMaskIntoConstraints = YES;
+    imageView2.translatesAutoresizingMaskIntoConstraints = YES;
+    imageView3.translatesAutoresizingMaskIntoConstraints = YES;
+    [self.scrollView addSubview:imageView1];
+    [self.scrollView addSubview:imageView2];
+    [self.scrollView addSubview:imageView3];
+    
+    float maxHeight = 0;
+    if (imageView1.bounds.size.height > imageView2.bounds.size.height && imageView1.bounds.size.height > imageView3.bounds.size.height) {
+        maxHeight = imageView1.bounds.size.height;
+    }else if (imageView2.bounds.size.height > imageView1.bounds.size.height && imageView2.bounds.size.height > imageView3.bounds.size.height) {
+        maxHeight = imageView2.bounds.size.height;
+    }else {
+        maxHeight = imageView3.bounds.size.height;
+    }
+    
+    CGSize size = CGSizeMake(imageView1.bounds.size.width+imageView2.bounds.size.width+imageView3.bounds.size.width, maxHeight);
+    
+    self.scrollView.contentSize = size;
+    
 }
+
 
 
 @end
